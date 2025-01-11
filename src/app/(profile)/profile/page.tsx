9cf3855500921084276902a3157e2d454834872f
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-01-11 16:06:27 +0700

    feat: Add profile management for user

---
+++ src/app/(profile)/profile/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from "next/navigation";
+
+import { findUserProfile } from "@/database/query";
+import { auth } from "@/lib/auth";
+import SiswaForm from "./_components/SiswaForm";
+import AdminStanForm from "./_components/AdminStanForm";
+
+const ProfilePage = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const userProfile = await findUserProfile({ id: session.user.id });
+
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {userProfile &&
+        (userProfile.role === "siswa" ? (
+          <SiswaForm siswa={userProfile.siswa ?? undefined} />
+        ) : (
+          <AdminStanForm stan={userProfile.stan ?? undefined} />
+        ))}
+    </main>
+  );
+};
+
+export default ProfilePage;
