84a0b6660f07cf313a1da4c0c326e3e467b6b168
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-01-11 16:06:41 +0700

    feat: Add footer and navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center p-2">
+      <p>©️ {new Date().getFullYear()} Web Kantin Negro</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 
+import { findSiswa } from "@/database/siswa";
 import { auth } from "@/lib/auth";
 
 interface Menu {
@@ -13,16 +15,8 @@ const menus: Menu[] = [
     url: "/",
   },
   {
-    title: "Sign Up",
-    url: "/signup",
-  },
-  {
-    title: "Sign In",
-    url: "/signin",
-  },
-  {
-    title: "Sign Out",
-    url: "/signout",
+    title: "Profile",
+    url: "/profile",
   },
   {
     title: "Siswa",
@@ -34,21 +28,87 @@ const menus: Menu[] = [
   },
 ];
 
+const authMenus: Menu[] = [
+  {
+    title: "Sign Up",
+    url: "/signup",
+  },
+  {
+    title: "Sign In",
+    url: "/signin",
+  },
+];
+
+const ProfilePicture = async ({ userId }: { userId: string }) => {
+  const userSiswa = await findSiswa({ userId });
+
+  return (
+    <Image
+      src={(userSiswa && userSiswa.foto) ?? "/dummy.jpg"}
+      alt="Profile Picture"
+      width={40}
+      height={40}
+      priority
+      className="aspect-square object-cover"
+    />
+  );
+};
+
 const Navbar = async () => {
   const session = await auth();
 
   return (
-    <nav>
-      <ul>
+    <nav className="w-full p-8">
+      <ul className="flex justify-between">
         <li>
-          <p>{session ? `Log in as ${session.user.username}` : "Not log in"}</p>
+          <ul className="flex gap-2">
+            {menus &&
+              menus.map((menu, i) => (
+                <li key={i}>
+                  <Link href={menu.url} className="block rounded-lg border p-2">
+                    {menu.title}
+                  </Link>
+                </li>
+              ))}
+          </ul>
         </li>
-        {menus &&
-          menus.map((menu, i) => (
-            <li key={i}>
-              <Link href={menu.url}>{menu.title}</Link>
-            </li>
-          ))}
+        <li>
+          <ul className="flex gap-2">
+            {session ? (
+              <>
+                <li>
+                  <Link href="/profile" className="block rounded-lg border p-2">
+                    {session.user.username}
+                  </Link>
+                </li>
+                {session.user.role === "siswa" && (
+                  <li>
+                    <Link
+                      href="/profile"
+                      className="block overflow-hidden rounded-full border"
+                    >
+                      <ProfilePicture userId={session.user.id} />
+                    </Link>
+                  </li>
+                )}
+                <li>
+                  <Link href="/signout" className="block rounded-lg border p-2">
+                    Sign Out
+                  </Link>
+                </li>
+              </>
+            ) : (
+              authMenus &&
+              authMenus.map((menu, i) => (
+                <li key={i}>
+                  <Link href={menu.url} className="block rounded-lg border p-2">
+                    {menu.title}
+                  </Link>
+                </li>
+              ))
+            )}
+          </ul>
+        </li>
       </ul>
     </nav>
   );
